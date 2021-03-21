65e9ced1f23cde3e6b342dcb25459843f58143f9
Nika <nikakw@gmail.com>
2021-03-21 01:22:22 +0000

    add bitwise_operators_O0 disassembly file, add n_lowest_nums_O0 disassembly file; update bitwise_operators.c source file

--- bitwise_operators/bitwise_operators.c
+++ bitwise_operators/bitwise_operators.c
@@ -1,66 +1,43 @@
 #include <stdio.h>
 #include <string.h>
 #include <math.h>
+#include <stdlib.h>
 
+int and_val(int a, int b){
+    return a&b;
+}
 
-int max_and(int a, int n, int k, int curr_max) {
-    int b = a + 1;
-    while ((a < b) && (b <= n)) {
-        while (b <= n) {
-            int new_and = a&b;
-            if ((new_and < k) && (new_and > curr_max)){
-                curr_max = new_and;
-            }
-            b++;
+int or_val(int a, int b){
+    return a|b;
+}
+
+int xor_val(int a, int b){
+    return a^b;
+}
+
+int max_val(int a, int n, int k, int curr_max, int(*bitwise_func)(int a, int b)){
+    while (a < n) {
+        int b = n;
+        while (b > a) {
+            int new_val = bitwise_func(a,b);
+            curr_max = ((new_val < k) && (new_val > curr_max)) ? new_val : curr_max;
+            b--;
         }
         a++;
-        b = a + 1;
     }
     return curr_max;
 }
 
-int max_or(int a, int n, int k, int curr_max_or) {
-    int b = a + 1;
-    while ((a < b) && (b <= n)) {
-        while (b <= n){
-            int new_or = a|b;
-            if ((new_or < k) && (new_or > curr_max_or)){
-                curr_max_or = new_or;
-            }
-            b++;
-        }
-        a++;
-        b = a + 1;
-    }
-    return curr_max_or;
-}
-
-int max_xor(int a, int n, int k, int curr_max_xor) {
-    int b = a + 1;
-    while ((a < b) && (b <= n)) {
-        while (b <= n) {
-            int new_xor = a^b;
-            if ((new_xor < k) && (new_xor > curr_max_xor)){
-                curr_max_xor = new_xor;
-            }
-            b++;
-        }
-        a++;
-        b = a + 1;
-    }
-    return curr_max_xor;
-}
-
-
 
 void calculate_the_maximum(int n, int k) {
-    int new_and_max = max_and(1, n, k, 0);
-    int new_or_max = max_or(1, n, k, 0);
-    int new_xor_max = max_xor(1, n, k, 0);
+    int new_and_max = max_val(1, n, k, 0, and_val);
+    int new_or_max = max_val(1, n, k, 0, or_val);
+    int new_xor_max = max_val(1, n, k, 0, xor_val);
     printf("%d \n", new_and_max);
     printf("%d \n", new_or_max);
     printf("%d \n", new_xor_max);
 }
+
 
 int main() {
     int n, k;
